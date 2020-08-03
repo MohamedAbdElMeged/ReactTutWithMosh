b19b92a3ac0609fa5d6c7d105d5315691c1cb93f
Mohamed Abdelmeged <mohamed.abdelmeged@nabdanet.com>
2020-08-03 23:20:07 +0200

    second commit

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,50 +1,13 @@
 import React, { Component } from 'react'
 import Counter from './counter';
 export class Counters extends Component {
-    state = {
-        counters: [
-            {
-                id : 1,
-                value: 0
-            },
-            {
-                id : 2,
-                value: 0
-            },
-            {
-                id : 3,
-                value: 0
-            },
-            {
-                id : 4,
-                value: 0
-            }
-        ]
-    }
 
-    handleIncrement = (counter) => {
-        //this.setState({counters: [...this.state.counters.map(counter => counter.value++)]});
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index].value++;
-        this.setState({counters});
-    }
-
-    handleReset = () => {
-        
-        this.setState({counters: [...this.state.counters.map(counter => {counter.value = 0;  return counter;})]});
-    }
-
-    handleDelete = (id) => {
-        //console.log("event handler called" , id);
-        this.setState({counters: [...this.state.counters.filter(counter => counter.id !== id)]})
-    }
     render() {
         return (
-            <div>
-                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
-                {this.state.counters.map(counter => (
-                     <Counter key={counter.id}  counter={counter} handleIncrement={this.handleIncrement}  handleDelete={this.handleDelete}>
+            <div className="container">
+                <button onClick={this.props.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
+                {this.props.counters.map(counter => (
+                     <Counter key={counter.id}  counter={counter} handleIncrement={this.props.handleIncrement}  handleDelete={this.props.handleDelete}>
                      </Counter>
                     ))}
             </div>
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                         <a class="navbar-brand" >Navbar</a>                        
+                    </nav>
+        )
+    }
+}
+
+export default Navbar
